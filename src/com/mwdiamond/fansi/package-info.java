f2ad402a06ebbeabd9cdfa15e23959acb178ef88
Michael Diamond <dimo414@gmail.com>
2016-02-18 17:39:45 -0500

    Added package-info file

---
+++ src/com/mwdiamond/fansi/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * F-Ansi, a Java terminal output library.
+ * 
+ * <p>The main entry-point class is {@link com.mwdiamond.fansi.Ansi}.
+ * See the <a href="https://bitbucket.org/dimo414/f-ansi">project page</a> for more details.
+ */
+package com.mwdiamond.fansi;
