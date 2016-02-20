46d2df3ffc639830b1a567fb9616a2c48493f430
Michael Diamond <dimo414@gmail.com>
2016-02-20 12:50:15 -0500

    Tests need to support Windows line separator

--- tests/com/mwdiamond/fansi/AnsiTest.java
+++ tests/com/mwdiamond/fansi/AnsiTest.java
@@ -21,7 +21,8 @@ import com.google.common.collect.ImmutableMap;
  * detector-style tests that may be brittle.
  */
 public class AnsiTest {
-    private static final String helloWorld = "Hello World";
+    private static final String HELLO = "Hello World";
+    private static final String LN = System.lineSeparator();
     private AnsiForTests ansiForTests;
 
     /**
@@ -47,30 +48,30 @@ public class AnsiTest {
 
     @Test
     public void plain() {
-        ansi().out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo(helloWorld);
+        ansi().out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo(HELLO);
         assertThat(ansiForTests.getStderr()).isEmpty();
     }
 
     @Test
     public void plainLn() {
-        ansi().outln(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo(helloWorld + "\n");
+        ansi().outln(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo(HELLO + LN);
         assertThat(ansiForTests.getStderr()).isEmpty();
     }
 
     @Test
     public void plainErr() {
-        ansi().err(helloWorld);
+        ansi().err(HELLO);
         assertThat(ansiForTests.getStdout()).isEmpty();
-        assertThat(ansiForTests.getStderr()).isEqualTo(helloWorld);
+        assertThat(ansiForTests.getStderr()).isEqualTo(HELLO);
     }
 
     @Test
     public void plainErrLn() {
-        ansi().errln(helloWorld);
+        ansi().errln(HELLO);
         assertThat(ansiForTests.getStdout()).isEmpty();
-        assertThat(ansiForTests.getStderr()).isEqualTo(helloWorld + "\n");
+        assertThat(ansiForTests.getStderr()).isEqualTo(HELLO + LN);
     }
 
     private static final Map<java.awt.Color, Integer> COLORS_TO_COLOR_INDEX =
@@ -92,77 +93,77 @@ public class AnsiTest {
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void setTitle() {
-        ansi().title(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e]0;" + helloWorld + "\\a");
+        ansi().title(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e]0;" + HELLO + "\\a");
     }
 
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void color() {
-        ansi().color(RED).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[31m" + helloWorld + "\\e[m");
+        ansi().color(RED).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[31m" + HELLO + "\\e[m");
     }
 
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void colorBackground() {
-        ansi().color(GREEN, RED).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[32;41m" + helloWorld + "\\e[m");
+        ansi().color(GREEN, RED).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[32;41m" + HELLO + "\\e[m");
     }
 
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void colorStyle() {
-        ansi().color(YELLOW, BOLD).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[1;33m" + helloWorld + "\\e[m");
+        ansi().color(YELLOW, BOLD).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[1;33m" + HELLO + "\\e[m");
     }
 
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void colorStyles() {
-        ansi().color(CYAN, BOLD, ITALIC).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[1;3;36m" + helloWorld + "\\e[m");
+        ansi().color(CYAN, BOLD, ITALIC).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[1;3;36m" + HELLO + "\\e[m");
     }
 
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void colorFont() {
-        ansi().color(BLUE, F1).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[11;34m" + helloWorld + "\\e[10m\\e[m");
+        ansi().color(BLUE, F1).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[11;34m" + HELLO + "\\e[10m\\e[m");
     }
 
     @Test
     public void chainedColor() {
-        ansi().color(BLUE).out(helloWorld).color(RED, GREEN).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[34m" + helloWorld + "\\e[m\\e[31;42m" + helloWorld + "\\e[m");
+        ansi().color(BLUE).out(HELLO).color(RED, GREEN).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[34m" + HELLO + "\\e[m\\e[31;42m" + HELLO + "\\e[m");
     }
 
     @Test
     @ChangeDetector(timesUpdated = 1)
     public void color8Bit() {
-        ansi().color(100, 200).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[38;5;100;48;5;200m" + helloWorld + "\\e[m");
+        ansi().color(100, 200).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[38;5;100;48;5;200m" + HELLO + "\\e[m");
     }
 
     @Test
     @ChangeDetector(timesUpdated = 1)
     public void color24Bit() {
-        ansi().color(java.awt.Color.RED, java.awt.Color.BLUE).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[38;2;255;0;0;48;2;0;0;255m" + helloWorld + "\\e[m");
+        ansi().color(java.awt.Color.RED, java.awt.Color.BLUE).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[38;2;255;0;0;48;2;0;0;255m" + HELLO + "\\e[m");
     }
 
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void moveCursor() {
-        ansi().moveCursor(10).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[10E" + helloWorld);
+        ansi().moveCursor(10).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[10E" + HELLO);
     }
 
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void moveCursorColumns() {
-        ansi().moveCursor(10, 10).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[10B\\e[10C" + helloWorld);
+        ansi().moveCursor(10, 10).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[10B\\e[10C" + HELLO);
     }
 
     @Test
@@ -176,31 +177,31 @@ public class AnsiTest {
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void fixed() {
-        ansi().fixed(10, 20).out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[s\\e[10;20H" + helloWorld + "\\e[u");
+        ansi().fixed(10, 20).out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[s\\e[10;20H" + HELLO + "\\e[u");
     }
 
     @Test
     public void fixedColor() {
-        ansi().fixed(10, 20).color(RED).outln(helloWorld);
-        ansi().color(RED).fixed(10, 20).outln(helloWorld);
+        ansi().fixed(10, 20).color(RED).outln(HELLO);
+        ansi().color(RED).fixed(10, 20).outln(HELLO);
         assertThat(ansiForTests.getStdout()).isEqualTo(
-            "\\e[s\\e[10;20H\\e[31m" + helloWorld + "\\e[m\\e[u\n" +
-            "\\e[31m\\e[s\\e[10;20H" + helloWorld + "\\e[u\\e[m\n"
+            "\\e[s\\e[10;20H\\e[31m" + HELLO + "\\e[m\\e[u" + LN +
+            "\\e[31m\\e[s\\e[10;20H" + HELLO + "\\e[u\\e[m" + LN
         );
     }
 
     @Test
     @ChangeDetector(timesUpdated = 1)
     public void overwriteThisLine() {
-        ansi().overwriteThisLine().out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[2K\\e[1G" + helloWorld);
+        ansi().overwriteThisLine().out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[2K\\e[1G" + HELLO);
     }
 
     @Test
     @ChangeDetector(timesUpdated = 0)
     public void overwriteLastLine() {
-        ansi().overwriteLastLine().out(helloWorld);
-        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[2K\\e[1F\\e[2K" + helloWorld);
+        ansi().overwriteLastLine().out(HELLO);
+        assertThat(ansiForTests.getStdout()).isEqualTo("\\e[2K\\e[1F\\e[2K" + HELLO);
     }
 }
